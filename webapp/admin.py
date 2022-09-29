8b807ef3b9e4929661f10f99484a085cc7852699
azamatdoronov <aza3el@inbox.ru>
2022-09-29 15:14:24 +0600

    Создал проект и приложение webapp, а также необходимые файлы для работы

---
+++ webapp/admin.py
@@ -0,0 +1,13 @@
+from django.contrib import admin
+
+from webapp.models import Product
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'amount', 'price')
+    list_display_links = ('pk', 'name')
+    list_filter = ('category',)
+    search_fields = ('name',)
+
+
+admin.site.register(Product, ProductAdmin)
